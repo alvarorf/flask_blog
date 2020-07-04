58e140e5d4ba295cbc846b266a4daf9d22ccc520
alvaro <aaruizf@unal.edu.co>
2020-07-04 13:23:02 -0400

    Added user authentication features

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '172213268cb8712da87df60b8c51bc42059dba741570741ab4d02cda24bf56f4b5e9250f2776e24a4d44af5d074588fcffb2f6e3be83962f8af248357c5fb8f1'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
